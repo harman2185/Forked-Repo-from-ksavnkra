9e74961185157f28872eda764aac77a134e1fc4a
Keshav Nakra <Keshav0796.be23@chitkara.edu.in>
2024-02-20 11:34:37 +0530

    Leet Code Question

---
+++ best_time_to_buy_sell_stocks.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    vector<int> nums{7, 1, 3, 6, 5, 4};
+    int min = INT_MAX;
+    int max = INT_MIN;
+    int buy_day, sell_day;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] < min)
+        {
+            min = nums[i];
+            buy_day = i;
+        }
+    }
+    for (int i = buy_day; i < nums.size(); i++)
+    {
+        if (nums[i] > max)
+        {
+            max = nums[i];
+            sell_day = i;
+        }
+    }
+    cout << "You should have bought on day " << buy_day + 1 << " when it was of Rs." << min << " and sell it on day " << sell_day + 1 << " when it was of Rs." << max;
+    return 0;
+}
