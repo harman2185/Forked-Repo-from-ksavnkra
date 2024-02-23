317c1108b82e9b634be6a2e7452bac01e06215f7
Keshav Nakra <Keshav0796.be23@chitkara.edu.in>
2024-02-23 13:46:30 +0530

    Commited Offline

--- best_time_to_buy_sell_stocks.cpp
+++ best_time_to_buy_sell_stocks.cpp
@@ -4,26 +4,6 @@
 using namespace std;
 int main()
 {
-    vector<int> nums{7, 1, 3, 6, 5, 4};
-    int min = INT_MAX;
-    int max = INT_MIN;
-    int buy_day, sell_day;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] < min)
-        {
-            min = nums[i];
-            buy_day = i;
-        }
-    }
-    for (int i = buy_day; i < nums.size(); i++)
-    {
-        if (nums[i] > max)
-        {
-            max = nums[i];
-            sell_day = i;
-        }
-    }
-    cout << "You should have bought on day " << buy_day + 1;
+    printf("Hello");
     return 0;
 }
